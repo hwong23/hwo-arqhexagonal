49b671bed9b5309056df0d8acf94cd06dbfff9fd
HWO <hwong23@gmail.com>
2023-12-26 02:18:32 +0000

    6

--- -Designing-Hexagonal-Architecture-with-Java---Second-Edition-main/Chapter01/src/main/java/dev/davivieira/App.java
+++ -Designing-Hexagonal-Architecture-with-Java---Second-Edition-main/Chapter01/src/main/java/dev/davivieira/App.java
@@ -8,7 +8,7 @@ public class App {
         var cli = new RouterViewCLIAdapter();
         var type = "CORE";
 
-        System.out.println("hola mundo 5");
+        System.out.println("hola mundo 6");
         System.out.println(cli.obtainRelatedRouters(type));
     }
 }
