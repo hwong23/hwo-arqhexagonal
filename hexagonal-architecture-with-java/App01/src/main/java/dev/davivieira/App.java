af3bde34636c3bb3b3630fe0dac2766c193c84a8
HWO <hwong23@gmail.com>
2024-01-08 20:18:49 -0500

    app1

---
+++ hexagonal-architecture-with-java/App01/src/main/java/dev/davivieira/App.java
@@ -0,0 +1,14 @@
+package dev.davivieira;
+
+import dev.davivieira.framework.adapters.input.stdin.RouterViewCLIAdapter;
+
+public class App {
+
+    public static void main(String... args) {
+        var cli = new RouterViewCLIAdapter();
+        var type = "CORE";
+
+        System.out.println("hola mundo 7");
+        System.out.println(cli.obtainRelatedRouters(type));
+    }
+}
