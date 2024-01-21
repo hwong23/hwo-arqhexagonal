6c15dd688468adc4866ee3ca1b89e8de0a541ece
hwong23 <hwong23@gmail.com>
2024-01-21 17:52:31 +0000

    upd

--- hexagonal-architecture-with-java/App01/src/main/java/dev/hwo/domain/Plan.java
+++
@@ -1,9 +0,0 @@
-package dev.hwo.domain;
-
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-public class Plan {
-    private final boolean conforme;
-}
