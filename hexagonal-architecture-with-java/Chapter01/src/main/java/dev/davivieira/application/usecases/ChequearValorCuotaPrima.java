cd2b72e00e3ea26dc8258759dd362db4dcbad182
hwong23 <hwong23@gmail.com>
2024-01-21 17:51:08 +0000

    primaUC

---
+++ hexagonal-architecture-with-java/Chapter01/src/main/java/dev/davivieira/application/usecases/ChequearValorCuotaPrima.java
@@ -0,0 +1,11 @@
+package dev.davivieira.application.usecases;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import dev.davivieira.domain.Prima;
+
+public class ChequearValorCuotaPrima {
+    boolean ValidarCuota (Predicate<Prima> filter);
+
+}
