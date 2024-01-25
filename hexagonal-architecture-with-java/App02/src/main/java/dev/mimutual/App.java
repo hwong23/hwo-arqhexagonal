21945ff097f9654e6e17f57e9efdb0586e383e99
hwong23 <hwong23@gmail.com>
2024-01-25 22:46:10 +0000

    pkg--app02

--- hexagonal-architecture-with-java/App02/src/main/java/dev/mimutual/App.java
+++ hexagonal-architecture-with-java/App02/src/main/java/dev/mimutual/App.java
@@ -7,6 +7,8 @@ public class App {
     public static void main(String... args) {
         var cli = new RouterViewCLIAdapter();
         var type = "CORE";
+
+        System.out.println( "App02.Referencia");
         System.out.println(cli.obtainRelatedRouters(type));
 
     }
--- hexagonal-architecture-with-java/Chapter01/src/main/java/dev/davivieira/application/usecases/ChequearValorCuotaPrima.java
+++ hexagonal-architecture-with-java/Chapter01/src/main/java/dev/davivieira/application/usecases/ChequearValorCuotaPrima.java
@@ -3,9 +3,7 @@ package dev.davivieira.application.usecases;
 import java.util.List;
 import java.util.function.Predicate;
 
-import dev.hwo.domain.Prima;
-
 public class ChequearValorCuotaPrima {
-    boolean ValidarCuota (Predicate<Prima> filter);
+    // boolean ValidarCuota (Predicate<Prima> filter);
 
 }
