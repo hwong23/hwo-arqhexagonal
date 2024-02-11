e9951b78ad66c6448e48ab19acbeffd8f40fc92c
HWO <hwong23@gmail.com>
2024-02-11 09:40:52 -0500

    init

--- Chapter01/src/main/java/dev/davivieira/application/usecases/ChequearValorCuotaPrima.java
+++
@@ -1,9 +0,0 @@
-package dev.davivieira.application.usecases;
-
-import java.util.List;
-import java.util.function.Predicate;
-
-public class ChequearValorCuotaPrima {
-    // boolean ValidarCuota (Predicate<Prima> filter);
-
-}
