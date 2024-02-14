b7f6f900f6b497c25c8aa6db1cfcda40fb91c2eb
hwong23 <hwong23@gmail.com>
2024-02-14 19:34:42 +0000

    upd--test

--- App04/src/test/java/dev/com/RunCucumberTest.java
+++ App04/src/test/java/dev/com/RunCucumberTest.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-result"}, features = "src/test/java/dev/davivieira/features")
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-result"}, 
+    features = "src/test/java/dev/com/features")
 public class RunCucumberTest {
 }
